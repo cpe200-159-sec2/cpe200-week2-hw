88687a8cc35970bb1eb246c880f760d6e5996937
beampanuwit <beampanuwit@hotmail.com>
2016-09-08 23:35:17 +0700

    so that is

--- src/cpe200/week2/PList.java
+++ src/cpe200/week2/PList.java
@@ -14,7 +14,12 @@ public class PList {
     }
 
     public void pushToTail(char i) {
-        // implement your code here
+        tail = new PNode(i, null,tail);
+        if (head==null)
+            tail = head;
+        else
+            tail.prev.next = tail;
+        size++;
     }
 
     public char popHead() {
@@ -35,11 +40,28 @@ public class PList {
     }
 
     public char popTail() {
-        // implement your code here
+        char i=tail.data;
+        PNode tmp=head;
+
+        if(tail == head){
+            head = tail =null;
+        }
+        else
+            tail =tail.prev;
+            tail.next = null;
+        size --;
+        return i;
     }
 
     public boolean search(char i) {
-        // implement your code here
+        PNode tmp =head;
+       while (tmp.next != null){
+           if (tmp.data != i){
+               tmp =tmp.next;
+               return true;
+           }
+       }
+       return  false;
     }
 
     public boolean isEmpty() { return (head == null); }
@@ -48,19 +70,28 @@ public class PList {
         PNode tmp = head;
 
         while (tmp != null) {
-            // implement your code here
+           tmp =tmp.prev;
+
         }
         System.out.println();
     }
 
     public void printBackward() {
-        // implement your code here
+        PNode tmp = tail;
+
+        while (tmp != null) {
+            tmp =tmp.prev;
+
+        }
+        System.out.println();
     }
+
 
     public int getSize() {
         return size;
     }
 
-    private PNode head, tail;
+    private PNode  head, tail;
     private int size=0;
+
 }
