6227acdec4002558c29caa75d6f7c9374ef34370
purrirorin <cc-aaa-tt@hotmail.com>
2016-09-09 04:58:56 +0700

    commitได้เหอะ

--- src/cpe200/week2/PList.java
+++ src/cpe200/week2/PList.java
@@ -2,11 +2,13 @@ package cpe200.week2;
 
 public class PList {
 
-    public PList() { head = tail = null; }
+    public PList() {
+        head = tail = null;
+    }
 
     public void pushToHead(char i) {
         head = new PNode(i, head, null);
-        if (tail==null)
+        if (tail == null)
             tail = head;
         else
             head.next.prev = head;
@@ -14,22 +16,21 @@ public class PList {
     }
 
     public void pushToTail(char i) {
-        if(head == null){
-                       head = new PNode(i, null, tail);
-                       head = tail;
-                   }
-        else{
-                      tail = new PNode(i, null, tail);
-                       tail.prev.next = tail;
-                   }
-               size++;
+        if (head == null) {
+            head = new PNode(i, null, tail);
+            head = tail;
+        } else {
+            tail = new PNode(i, null, tail);
+            tail.prev.next = tail;
+        }
+        size++;
     }
 
     public char popHead() {
-        char i=head.data;
+        char i = head.data;
         PNode tmp = head;
 
-        if (head==tail)
+        if (head == tail)
             head = tail = null;
         else {
             head = head.next;
@@ -48,34 +49,39 @@ public class PList {
 
     public boolean search(char i) {
         PNode tmp = head;
-               while (tmp.next != null) {
-                       tmp = tmp.next;
-              char i = tmp.data;
-                tmp.prev.next = null;
-               size--;
-               return i;
-    }
+        while (tmp.next != null) {
+            tmp = tmp.next;
+            char i = tmp.data;
+            tmp.prev.next = null;
+            size--;
+            return i;
+        }
 
-    public boolean isEmpty() { return (head == null); }
+    public boolean isEmpty() {
+        return (head == null);
+    }
 
     public void printForward() {
         PNode tmp = head;
 
         while (tmp != null) {
             System.out.print(tmp.data);
-                      tmp = tmp.next;
+            tmp = tmp.next;
         }
         System.out.println();
     }
 
     public void printBackward() {
-        PNode tmp = tail;
-
-                       while (tmp != null){
-                      System.out.print(tmp.data);
-                       tmp = tmp.prev;
-                   }
-               System.out.println();
+        PNode tmp = head;
+        while (tmp.next != null) {
+            tmp = tmp.next;
+        }
+        System.out.print(tmp.data);
+        while (tmp != head) {
+            System.out.print(tmp.prev.data);
+            tmp = tmp.prev;
+        }
+        System.out.println();
     }
 
     public int getSize() {
@@ -83,5 +89,5 @@ public class PList {
     }
 
     private PNode head, tail;
-    private int size=0;
+    private int size = 0;
 }
